3261eb9a2807ef898d25f444eebc47310e34e869
Joe Panetta <jlapnetta1681@icloud.com>
2021-01-20 23:12:36 -0500

    creating layout in studentdash.js to start rendering the externall links with anchor tags

---
+++ src/components/pages/Headmaster/Student/StudentDash.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './StudentDashboard.css';
+
+function StudentDashboard() {
+  return <div>Welcome to the student dashboard</div>;
+}
+
+export default StudentDashboard;
