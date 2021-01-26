625147243c784841a66411af6422ed8af21bd5e7
Joe Panetta <jlapnetta1681@icloud.com>
2021-01-26 17:32:49 -0500

    fixed logos and hyperlinks for resource dash.

--- src/components/pages/Student/StudentResourceDash.js
+++ src/components/pages/Student/StudentResourceDash.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 function StudentDashboard() {
   return (
     <>
@@ -10,61 +9,52 @@ function StudentDashboard() {
       </div>
 
       <div className="student-dash-middle">
-
         <div classsName="resource-link">
-
-          <a href="https://www.google.com">Click here to learn almost anything.
-                    <img src="https://cdn3.iconfinder.com/data/icons/social-network-30/512/social-09-128.png" alt="google-logo" />
+          <a href="https://www.google.com">
+            Click here to learn almost anything.
+            <img src="public/images/glogo.png" alt="google-logo" />
           </a>
         </div>
-        <div classsName="resource-link">
-
-          <a href="https://www.notion.com">Click here to visit notion.
-                    <img src="https://cdn.iconscout.com/icon/free/png-128/notion-1693557-1442598.png" alt="noition-logo" />
+        <div classsName="resource link">
+          <a href="https://www.notion.com">
+            Click here to visit notion.
+            <img
+              src="public/images/notion-1693557-1442598.jpg.png"
+              alt="noition-logo"
+            />
           </a>
         </div>
 
-
         <div className="resource-link">
-          <a href="https://www.khanacademy.org/"> Click here to visit Khan Academy.
-                    </a>
-
-          <image src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/khanacademy-128.png" alt="khan-logo" />
+          <a href="https://www.khanacademy.org/">
+            {' '}
+            Click here to visit Khan Academy.
+            <img src="public/images/khanlogo.jpg.jpg" alt="khan-logo" />
+          </a>
         </div>
 
-
-
-
-
-
         <div className="resource-link">
-          <a href="https://www.k5learning.com/">Visit k-5learning.
-                    <image src="k5learning logo here" />
-
+          <a href="https://www.k5learning.com/">
+            Visit k-5learning.
+            <img src="k5learning logo here" alt="k5-logo" />
           </a>
         </div>
 
         <div>
-          <a href="https://www.ixl.com/">Click Here t ovisit IXL.com"
-                <image
-              src="../../public/images/ixllogo.jpg.png" />
+          <a href="https://www.ixl.com/">
+            Click Here t ovisit IXL.com"
+            <img src="public/images/ixllogo.png" alt="ixl logo" />
           </a>
         </div>
 
-
-
         <div className="resource-link">
-          <a href="https://www.math-drills.com/">Takle me to Math-drills!
-                <image
-              src=" />
+          <a href="https://www.math-drills.com/">
+            Takle me to Math-drills!
+            <img src="public/images/mathlogo.jpg" alt="math drills logo." />
           </a>
         </div>
       </div>
     </>
-
-
-
-
   );
 }
 
